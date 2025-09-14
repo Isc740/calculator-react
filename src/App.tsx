4c88b5085d3a25cc53f1c8a4b07d10fdc04516c6
Isc740 <isaazcantillo@gmail.com>
2025-09-14 18:39:14 -0500

    change style of ButtonContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-
+import ButtonContainer from "./components/ButtonContainer"
 
 function App() {
   return (
-    <div>
+    <div className="mx-auto my-5 border rounded shadow p-4" style={{ maxWidth: "350px" }}>
+      <ButtonContainer handleOnClick={() => { console.log("click!") }} />
     </div>
   )
 }
